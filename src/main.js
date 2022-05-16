b4647bfc5474d4fada34250265f194562da75405
ErickaAndreinaCortes <erickaandreinacortes@gmail.com>
2022-05-16 11:45:26 -0400

    filtrado

--- src/main.js
+++ src/main.js
@@ -9,5 +9,12 @@ const potions = data.potions;
 
 
 //console.log(characters); // console: ['0', '1', '2']
-const busqueda = characters.filter(item => item.patronus)
+const busqueda = characters.filter(item => item.associated_groups ==
+    "Potter family")
+
+//return house = Gryffindor
+
 console.log(busqueda);
+
+/*const casa = busqueda.filter(item => item.Gryffindor)
+console.log(casa);*/
