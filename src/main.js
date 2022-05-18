31c99ed9505e13bb1f7df1a269d23d7a4013ad18
ErickaAndreinaCortes <erickaandreinacortes@gmail.com>
2022-05-18 12:53:38 -0400

    cambios

--- src/main.js
+++ src/main.js
@@ -9,25 +9,32 @@ const potions = data.potions;
 
 
 //console.log(characters); // console: ['0', '1', '2']
-const busqueda = characters.filter(item => item.id ==
-    "326" || item.id == "476" || item.id == "587" ||
-    item.id == "472" || item.id == "473" ||
-    item.id == "258" || item.id == "117" ||
-    item.id == "743" || item.id == "267" ||
-    item.id == "254" || item.id == "756" ||
-    item.id == "744" || item.id == "123" ||
-    item.id == "327" || item.id == "679" ||
-    item.id == "382" || item.id == "350")
-
+const busqueda = () => {
+        characters.filter(item => item.id ==
+            "326" || item.id == "476" || item.id == "587" ||
+            item.id == "472" || item.id == "473" ||
+            item.id == "258" || item.id == "117" ||
+            item.id == "743" || item.id == "267" ||
+            item.id == "254" || item.id == "756" ||
+            item.id == "744" || item.id == "123" ||
+            item.id == "327" || item.id == "679" ||
+            item.id == "382" || item.id == "350")
+    }
+    //console.log(typeof busqueda);
 let dataOriginal = [];
 dataOriginal.push(busqueda);
 
+const item = document.getElementById("item1");
+dataOriginal.forEach(elementos => {
+    item.innerHTML = `<li>${elementos.name} </li>`
+})
+console.log(item);
 
-console.log(dataOriginal);
 
-let item1 = document.getElementById ("item1")
 
-item1.addEventListener ("click"); 
+/*let item1 = document.getElementById ("item1")
+item1.addEventListener ("click"); */
+
 
 
 /*const casa = busqueda.filter(item => item.Gryffindor)
