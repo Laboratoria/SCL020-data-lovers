6006570eacaafae5431ad91c56541d5b212d2d63
ErickaAndreinaCortes <erickaandreinacortes@gmail.com>
2022-05-18 18:42:09 -0400

    cambios y mas cambios

--- src/data.js
+++ src/data.js
@@ -15,3 +15,18 @@ export const anotherExample = () => {
     })
 }
 console.log(search('potter'));*/
+
+export const busqueda = (arrayCharacters) => {
+    //console.log(arrayCharacters)
+    const arrayFiltradoPorID = arrayCharacters.filter((item) => item.id ==
+        "326" || item.id == "476" || item.id == "587" ||
+        item.id == "472" || item.id == "473" ||
+        item.id == "258" || item.id == "117" ||
+        item.id == "743" || item.id == "267" ||
+        item.id == "254" || item.id == "756" ||
+        item.id == "744" || item.id == "123" ||
+        item.id == "327" || item.id == "679" ||
+        item.id == "382" || item.id == "350");
+    return arrayFiltradoPorID;
+
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { example } from './data.js';
+import { busqueda } from './data.js';
+
+import { mostrarData } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/harrypotter/data.js';
 const characters = data.characters;
@@ -9,40 +11,24 @@ const potions = data.potions;
 
 
 //console.log(characters); // console: ['0', '1', '2']
-const busqueda = () => {
-        characters.filter(item => item.id ==
-            "326" || item.id == "476" || item.id == "587" ||
-            item.id == "472" || item.id == "473" ||
-            item.id == "258" || item.id == "117" ||
-            item.id == "743" || item.id == "267" ||
-            item.id == "254" || item.id == "756" ||
-            item.id == "744" || item.id == "123" ||
-            item.id == "327" || item.id == "679" ||
-            item.id == "382" || item.id == "350")
-    }
-    //console.log(typeof busqueda);
+
+//console.log(busqueda(characters));
+
 let dataOriginal = [];
 dataOriginal.push(busqueda);
 
-const item = (dataOriginal) => {
-    dataOriginal.forEach(prueba => {
-        console.log(dataOriginal.name);
-    });
-}
+
+
+
+
+
+
+
+
+
 
 /*const item = document.getElementById("item1");
 dataOriginal.forEach(elementos => {
     item.innerHTML = `<li>${elementos.name} </li>`
 })
 console.log(item);*/
-
-
-
-/*let item1 = document.getElementById ("item1")
-item1.addEventListener ("click"); */
-
-
-
-/*const casa = busqueda.filter(item => item.Gryffindor)
-return house = Gryffindor
-console.log(casa); esta no funciono ya que estamos llamando a gryffindor que es un valor*/
